a614dfefc6e07ef7eca537590609e3fb7c3533b3
John Howe <johnhowe@gmail.com>
2011-02-13 22:30:43 +1300

    Flashing LEDs

---
+++ config.h
@@ -0,0 +1,11 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#include <msp430x20x2.h>
+
+#define RED_LED     BIT0
+#define GREEN_LED   BIT6
+#define RX_PIN      BIT5
+
+
+#endif
--- main.c
+++ main.c
@@ -1,10 +1,25 @@
-#include <msp430x20x2.h>
+/**
+ * Simple AFR gauge using an MSP430 and a 7-segment serial display.
+ */
 
+#include <msp430x20x2.h>
+#include "config.h"
+
+// A delay of 100000 is roughly one second.
+void delay (unsigned int time){
+    while (time > 0) {
+        time--;
+    }
+}
 
 int main() {
+    WDTCTL = WDTPW + WDTHOLD;
+    P1OUT = RED_LED;
+    P1DIR |= RX_PIN | RED_LED | GREEN_LED;
 
-    for (;;) {
-
+    while (1) {
+        delay (60000);
+        P1OUT ^= GREEN_LED | RED_LED;
     }
     return 0;
 }
