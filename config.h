32664e30fd8219c8b4e2f1497c65fdbacd0dfa36
John Howe <johnhowe@gmail.com>
2011-02-21 00:04:07 +1300

    Added LCD commands

--- config.h
+++ config.h
@@ -11,4 +11,21 @@
 #define MOSI_PIN        BIT4        // Master out, slave in
 #define CLK_PIN         BIT5        // Clock
 
+/* LCD Commands */
+#define DECIMAL         (0x77)
+#define BRIGHTNESS      (0x7a)
+#define RESET           (0x76)
+#define DIGIT1          (0x7b)
+#define DIGIT2          (0x7c)
+#define DIGIT3          (0x7d)
+#define DIGIT4          (0x7e)
+
+/* Decimal points */
+#define DECIMAL0        (1<<0)
+#define DECIMAL1        (1<<1)
+#define DECIMAL2        (1<<2)
+#define DECIMAL3        (1<<3)
+#define COLON           (1<<4)
+#define APOSTROPHE      (1<<5)
+
 #endif
